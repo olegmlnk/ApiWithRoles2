3857fbb1afbacb6f473edf4b869dce0f414f3e82
oleg_melnyk <olegmelnyk237@gmail.com>
2025-04-26 23:55:34 +0300

    added logindto and registerdto

---
+++ ApiWithRoles2.Application/DTO/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiWithRoles2.Application.DTO
+{
+    public class Register
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- ApiWithRoles2.Application/Services/UserService.cs
+++ ApiWithRoles2.Application/Services/UserService.cs
@@ -1,6 +1,9 @@
-﻿namespace ApiWithRoles2.Application.Services
+﻿using ApiWithRoles2.Domain.Models;
+
+namespace ApiWithRoles2.Application.Services
 {
     public class UserService
     {
+        private readonly UserService<ApplicationUser> _userManager;
     }
 }
